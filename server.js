830e945baf86cef327ff7b8ff31e07bd0e92326d
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2018-08-01 00:25:31 +0800

    added nodeman

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser');
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
@@ -10,6 +12,7 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res) {
   res.render('index');
+  console.log(req.body.city);
 })
 
 app.listen(3000, function () {
