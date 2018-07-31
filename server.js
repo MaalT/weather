242269cc33d083961b21b8fd6292a0d0ba74633c
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2018-08-01 04:26:01 +0800

    fixed weather stringify and api keys

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
+
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+const apiKey = 'e3cce80cfb2e503b8549feffd23a1f8c';
+const dotenv = require('dotenv').config();
 
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {	
   res.render('index');
+
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&units=imperial&appid=' + apiKey;
+
+	request(url, function (err, response, body) {
+    if(err){    	
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      console.log(weather)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = 'It is '+ weather.main.temp + ' degrees in '+ weather.name + ' !';
+        res.render('index', {        	
+        	error: null,
+        	weather : JSON.stringify(weather),
+        	weather: weatherText, 
+        });
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
