e43d9984bfa51f1bdbacccd2b1d2402f3977642e
Michelle Ler <michelleler@Michelles-MacBook-Pro-2.local>
2018-09-22 09:29:23 +0800

    edit port

--- server.js
+++ server.js
@@ -39,6 +39,6 @@ app.post('/weather', function (req, res) {
   });
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
